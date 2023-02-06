35f88fca25020a9822bc751831503e708b647c8c
D55ma <100362815+D55ma@users.noreply.github.com>
2023-02-06 15:44:55 +0300

    Update test2.java

--- test2.java
+++ test2.java
@@ -1,7 +1,15 @@
 
-public Boolean checkSomething() {
+
+class test2 {
+	public static void main(String args[])
+	{
+		int x=7;
+        
+		public static Boolean checkSomething() {
     if (x>3) {
         boolean boolValue =true;
         return boolValue;
     } else return null;
+}
+	}
 }
